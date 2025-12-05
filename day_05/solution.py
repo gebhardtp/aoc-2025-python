c91fbfbb09a45166a8e5ff414d3b0b5ca7085c1d
gebhardtp <gebhardtp@proton.me>
2025-12-05 14:53:18 +0100

    add solution for day 05 part 2

--- day_05/solution.py
+++ day_05/solution.py
@@ -7,7 +7,8 @@ def read_input(filepath):
 def part_01(database):
     number_of_fresh_ingredients = 0
 
-    fresh_ranges, available_ids = read_fresh_and_available(database)
+    fresh_ranges = read_fresh(database)
+    available_ids = read_available(database)
 
     for id in available_ids:
         found_in_range = False
@@ -21,24 +22,55 @@ def part_01(database):
 
     return number_of_fresh_ingredients
 
-def read_fresh_and_available(database):
-    reading_ranges = True
-    fresh_ranges = []
+def part_02(database):
+    number_of_fresh_ids = 0
+    fresh_ranges = read_fresh(database)
+    fresh_ranges.sort()
+
+    merged_ranges = merge_overlapping_ranges(fresh_ranges, 0)
+
+    for merged_range in merged_ranges:
+        number_of_fresh_ids += merged_range[1] - merged_range[0] + 1
+    
+    return number_of_fresh_ids
+
+def merge_overlapping_ranges(fresh_ranges, index=0):
+    if index >= len(fresh_ranges) - 1:
+        return fresh_ranges
+    
+    if fresh_ranges[index][1] >= fresh_ranges[index + 1][0]:
+        new_end = max(fresh_ranges[index][1], fresh_ranges[index + 1][1])
+        fresh_ranges[index] = (fresh_ranges[index][0], new_end)
+        fresh_ranges.pop(index + 1)
+    else:
+        index += 1
+
+    return merge_overlapping_ranges(fresh_ranges, index)
+
+
+def read_available(database):
+    read_ids = False
     available_ids = []
     for line in database:
-        if line == "":
-            reading_ranges = False
-            continue
-        if reading_ranges:
-            fresh_range = line.split("-")
-            fresh_ranges.append((int(fresh_range[0]), int(fresh_range[1])))
-        else:
+        if read_ids:
             available_ids.append(int(line))
-    return fresh_ranges, available_ids
+        elif line == "":
+            read_ids = True
+    return available_ids
+
+def read_fresh(database):
+    fresh_ranges = []
+    for line in database:
+        if line == "":
+            break
+        fresh_range = line.split("-")
+        fresh_ranges.append((int(fresh_range[0]), int(fresh_range[1])))
+    return fresh_ranges
 
 def main():
     database = read_input("day_05/input.txt")
     print(f"Fresh ingredients: {part_01(database)}")
+    print(f"Fresh IDs: {part_02(database)}")
 
 if __name__ == "__main__":
     main()
