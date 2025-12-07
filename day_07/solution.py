43d7a53928ce3c75092adade0545f6e21cca1838
gebhardtp <gebhardtp@proton.me>
2025-12-07 16:24:25 +0100

    add solution day 07 part 2

--- day_07/solution.py
+++ day_07/solution.py
@@ -1,5 +1,7 @@
 # --- Day 7: Laboratories ---
     
+import time
+
 def part_01(diagram):
     total_splits = 0
     for line_index in range(1, len(diagram)):
@@ -25,10 +27,40 @@ def print_diagram_lines(diagram_lines):
     for line in diagram_lines:
         print(line)
 
+def part_02(diagram):
+    possible_paths = 0
+
+    rows = len(diagram)
+    collumns = len(diagram[0])
+    diagram_values = [[0 for _ in range(collumns)] for _ in range(rows)]
+
+    for row in range(rows):
+        for collumn in range(collumns):
+            if diagram[row][collumn] == "S":
+                diagram_values[row][collumn] = 1
+
+    for row in range(rows - 1):
+        for collumn in range(collumns):
+            if diagram_values[row][collumn] > 0:
+                current_tile = diagram[row][collumn]
+                if current_tile == '.' or current_tile == 'S':
+                    diagram_values[row+1][collumn] += diagram_values[row][collumn]
+                elif current_tile == '^':
+                    if collumn > 0:
+                        diagram_values[row+1][collumn-1] += diagram_values[row][collumn]
+                    if collumn < collumns - 1:
+                        diagram_values[row+1][collumn+1] += diagram_values[row][collumn]
+    
+    possible_paths = sum(diagram_values[-1])
+    return possible_paths
+
+def read_input(filepath):
+    with open(filepath) as f:
+        return f.read().splitlines()
+
 def main():
-    with open("day_07/test_input.txt") as file:
-        diagram = file.read().splitlines()
-        print(f"\nTotal splits: {part_01(diagram)}\n")
+    print(f"\nTotal splits: {part_01(read_input("day_07/input.txt"))}")
+    print(f"\nPossible paths: {part_02(read_input("day_07/input.txt"))}\n")
 
 if __name__ == "__main__":
     main()
