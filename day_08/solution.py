7494cfe88fe05b31310f175522bc2a9092341173
gebhardtp <gebhardtp@proton.me>
2025-12-12 16:33:08 +0100

    add solution day 08 part 2

--- day_08/solution.py
+++ day_08/solution.py
@@ -35,6 +35,38 @@ def part_01(points_str_list):
 
     return sizes[0] * sizes[1] * sizes[2]
 
+def part_02(points_str_list):
+    points = [ast.literal_eval(point) for point in points_str_list]
+    circuits = []
+    distance = calculate_distances(points)
+
+    distance_list = sorted(distance.items(), key=lambda x: x[1])
+    pair_list = [pair_and_distance[0] for pair_and_distance in distance_list]
+
+    final_pair = None
+
+    for pair in pair_list:
+        found_indices = []
+        for i, circuit in enumerate(circuits):
+            if pair[0] in circuit or pair[1] in circuit:
+                found_indices.append(i)
+        
+        if not found_indices:
+            circuits.append(set(pair))
+        elif len(found_indices) == 1:
+            circuits[found_indices[0]].update(pair)
+        else:
+            idx_keep = found_indices[0]
+            idx_remove = found_indices[1]
+            circuits[idx_keep].update(circuits[idx_remove])
+            circuits.pop(idx_remove)
+            if len(circuits) != 0 and len(circuits) <= 1:
+                final_pair = pair
+                break
+    
+    result = final_pair[0][0] * final_pair[1][0]
+    return result
+
 def calculate_distances(points):
     distance = {}
     for point_a in points:
@@ -53,6 +85,7 @@ def read_input(filepath):
 def main():
     junctions_coordinates = read_input("day_08/input.txt")
     print(part_01(junctions_coordinates))
+    print(part_02(junctions_coordinates))
 
 if __name__ == "__main__":
     main()
