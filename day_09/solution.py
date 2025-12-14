6d42c36bd35ba1b1801aa1948d89642540e61f1e
gebhardtp <gebhardtp@proton.me>
2025-12-14 13:18:38 +0100

    add solution day 09 part 2

--- day_09/solution.py
+++ day_09/solution.py
@@ -1,5 +1,8 @@
 # --- Day 9: Movie Theater ---
 
+import ast
+from shapely.geometry import Polygon, box
+
 def part_01(red_tiles):
     largest_area = float('-inf')
 
@@ -20,14 +23,39 @@ def get_rectangle_area(tile_a, tile_b):
     side_b = abs(int(corner_a[1]) - int(corner_b[1])) + 1
     return side_a * side_b
 
+def part_02(red_tiles):
+    largest_area = float('-inf')
+    points = [ast.literal_eval(point) for point in red_tiles]
+    main_polygon = Polygon(points)
+
+    for point_a in points:
+        for point_b in points:
+            if point_a == point_b:
+                continue
+            width = abs(point_a[0] - point_b[0]) + 1
+            height = abs(point_a[1] - point_b[1]) + 1
+            area = width * height
+            
+            if area > largest_area:
+                min_x = min(point_a[0], point_b[0])
+                max_x = max(point_a[0], point_b[0])
+                min_y = min(point_a[1], point_b[1])
+                max_y = max(point_a[1], point_b[1])
+                rectangle = box(min_x, min_y, max_x, max_y)
+                if main_polygon.covers(rectangle):
+                    largest_area = area
+
+    return largest_area
+
 def read_input(filepath):
     with open(filepath) as f:
         return f.read().splitlines()
 
 def main():
     red_tiles = read_input('day_09/input.txt')
-    result = part_01(red_tiles)
-    if result: print(result)
+    # if result := part_01(red_tiles): print(result)
+    if result := part_02(red_tiles): print(result)
+    
 
 if __name__ == '__main__':
     main()
